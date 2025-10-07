7c5c5b11c7e128215da7c6a3647ad20664fa80a5
duytran1104 <duy.tran1811oto@hcmut.edu.vn>
2025-10-07 23:55:39 +0700

    init for Ex9

---
+++ Lab2/Core/Inc/software_timer.h
@@ -0,0 +1,23 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 6, 2025
+ *      Author: ADMIN-5540
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+
+extern int timer0_flag;
+void setTimer0(int duration);
+
+extern int timer2_flag;
+void setTimer2(int duration);
+
+void timerRun();
+void timer_Run();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
